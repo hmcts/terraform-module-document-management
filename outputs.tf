2115f8b76b652803170c3b01cb8d5c4f44de8c3e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-25 12:51:35 +0000

    add more outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "ai_foundry_identity" {
 output "ml_workspace_identity" {
   value = azurerm_machine_learning_workspace.ml_workspace.identity[0].principal_id
 }
+
+output "cognitive_account_primary_access_key" {
+  value = azurerm_cognitive_account.cognitive_account.primary_access_key
+}
+
+output "cognitive_account_secondary_access_key" {
+  value = azurerm_cognitive_account.cognitive_account.secondary_access_key
+}
