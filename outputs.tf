45a5680e3d44ae1cf0ad51adc0cbe9aa7baae1c0
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-25 09:31:28 +0000

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
-output "resource_group_name" {
-  value = "resource_group_name"
+output "cognitive_account_identity" {
+  value = azurerm_cognitive_account.cognitive_account.identity[0].principal_id
 }
 
-output "resource_group_location" {
-  value = "resource_group_location"
+output "ai_services_identity" {
+  value = azurerm_ai_services.ai_services.identity[0].principal_id
+}
+
+output "ai_foundry_identity" {
+  value = azurerm_ai_foundry.ai_foundry.identity[0].principal_id
+}
+
+output "ml_workspace_identity" {
+  value = azurerm_machine_learning_workspace.ml_workspace.identity[0].principal_id
 }
