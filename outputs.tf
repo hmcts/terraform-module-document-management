0283ddf3db4a76250dbcd76b1a896caccc143713
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-25 11:07:56 +0000

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "cognitive_account_identity" {
-  value = azurerm_cognitive_account.cognitive_account.identity[0].principal_id
+  value = azurerm_cognitive_account.cognitive_account.identity.principal_id
 }
 
 output "ai_foundry_identity" {
-  value = azurerm_ai_foundry.ai_foundry.identity[0].principal_id
+  value = azurerm_ai_foundry.ai_foundry.identity.principal_id
 }
 
 output "ml_workspace_identity" {
-  value = azurerm_machine_learning_workspace.ml_workspace.identity[0].principal_id
+  value = azurerm_machine_learning_workspace.ml_workspace.identity.principal_id
 }
