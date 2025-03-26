afa9c78497a2850d442cb941a38428b276fe8da5
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-26 14:54:41 +0000

    add private endpoint

--- network.tf
+++ network.tf
@@ -1,4 +1,26 @@
-resource "azurerm_private_endpoint" "private_endpoint" {
+resource "azurerm_private_endpoint" "foundry_private_endpoint" {
+  name                = "${var.product}-${var.component}-ai-foundry-pe-${var.env}"
+  location            = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
+  resource_group_name = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "ai-foundry-psc"
+    private_connection_resource_id = azurerm_ai_foundry.ai_foundry.id
+    subresource_names              = ["amlworkspace"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name = "endpoint-dnszonegroup"
+    private_dns_zone_ids = [
+      "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.api.azureml.ms",
+      "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.notebooks.azure.net"
+    ]
+  }
+}
+
+resource "azurerm_private_endpoint" "ml_private_endpoint" {
   name                = "${var.product}-${var.component}-ml-workspace-pe-${var.env}"
   location            = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
   resource_group_name = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
