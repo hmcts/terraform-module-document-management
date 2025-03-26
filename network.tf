af16959a4132ea3bfc7e96e6fa5e907641ba75e7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-26 14:32:53 +0000

    add private endpoint to ml

---
+++ network.tf
@@ -0,0 +1,21 @@
+resource "azurerm_private_endpoint" "private_endpoint" {
+  name                = "${var.product}-${var.component}-ml-workspace-pe-${var.env}"
+  location            = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
+  resource_group_name = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "ml-workspace-psc"
+    private_connection_resource_id = azurerm_machine_learning_workspace.ml_workspace.id
+    subresource_names              = ["amlworkspace"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name = "endpoint-dnszonegroup"
+    private_dns_zone_ids = [
+      "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.api.azureml.ms",
+      "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.notebooks.azure.net"
+    ]
+  }
+}
