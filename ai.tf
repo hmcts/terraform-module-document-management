f468bbf792341be30106fbb691e55b6c3e70202c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-31 09:03:20 +0100

    set public access to disabled

--- ai.tf
+++ ai.tf
@@ -1,9 +1,10 @@
 resource "azurerm_ai_foundry" "ai_foundry" {
-  name                = "${var.product}-ai-foundry-${var.env}"
-  location            = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
-  resource_group_name = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
-  storage_account_id  = var.storage_account_id
-  key_vault_id        = var.key_vault_id
+  name                          = "${var.product}-ai-foundry-${var.env}"
+  location                      = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
+  resource_group_name           = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
+  storage_account_id            = var.storage_account_id
+  key_vault_id                  = var.key_vault_id
+  public_network_access_enabled = false
 
   identity {
     type = "SystemAssigned"
@@ -17,10 +18,11 @@ resource "azurerm_ai_foundry" "ai_foundry" {
 }
 
 resource "azurerm_cognitive_account" "cognitive_account" {
-  name                = "${var.product}-cognitive-account-${var.env}"
-  location            = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
-  resource_group_name = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
-  kind                = var.cognitive_account_kind
+  name                          = "${var.product}-cognitive-account-${var.env}"
+  location                      = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
+  resource_group_name           = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
+  kind                          = var.cognitive_account_kind
+  public_network_access_enabled = false
 
   sku_name = var.cognitive_account_sku
 
@@ -33,12 +35,13 @@ resource "azurerm_cognitive_account" "cognitive_account" {
 }
 
 resource "azurerm_machine_learning_workspace" "ml_workspace" {
-  name                    = "${var.product}-ml-workspace-${var.env}"
-  location                = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
-  resource_group_name     = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
-  application_insights_id = var.application_insights_id
-  key_vault_id            = var.key_vault_id
-  storage_account_id      = var.storage_account_id
+  name                          = "${var.product}-ml-workspace-${var.env}"
+  location                      = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].location : var.location
+  resource_group_name           = var.existing_resource_group_name == null ? azurerm_resource_group.rg[0].name : var.existing_resource_group_name
+  application_insights_id       = var.application_insights_id
+  key_vault_id                  = var.key_vault_id
+  storage_account_id            = var.storage_account_id
+  public_network_access_enabled = false
 
   identity {
     type = "SystemAssigned"
